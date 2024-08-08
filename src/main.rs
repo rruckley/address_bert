2ae5b59b1ae9d883bfb5078a3d24fde23f79aa40
Ryan Ruckley <rruckley@gmail.com>
2024-08-09 09:53:38 +1000

    BAsic structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 
 use candle_core::{Device, Result, Tensor};
 use log::info;
+use hf_hub::api::sync::Api;
+
+
+struct Model {
+    first: Tensor,
+    second: Tensor,
+}
+
+impl Model {
+    fn forward(&self, image: &Tensor) -> Result<Tensor> {
+        let x = image.matmul(&self.first)?;
+        let x = x.relu()?;
+        x.matmul(&self.second)
+    }
+}
 
 fn main() -> Result<()> {
     env_logger::init();
@@ -9,6 +24,8 @@ fn main() -> Result<()> {
     let ver = env!("CARGO_PKG_VERSION");
 
     info!("Starting {pkg} v{ver}");
+
+    let start = std::time::Instant::now();
 
     Ok(())
 }
