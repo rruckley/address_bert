0ac1f19587cc78621bfe7f2ed66962374d64c119
Ryan Ruckley <rruckley@gmail.com>
2025-01-06 14:06:23 +1100

    feat: Basic functions.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,54 @@
 
-use candle_core::{Device, Result, Tensor};
+use candle_core::{DType,Device, Tensor};
+use candle_nn::VarBuilder;
+use candle_transformers::models::distilbert::{Config,DistilBertModel};
 use log::info;
-use hf_hub::api::sync::Api;
+use tokenizers::{Tokenizer,TokenizerImpl};
+use hf_hub::{api::sync::Api, Repo, RepoType};
+use anyhow::{Error, Result};
 
+fn build_model() -> Result<(DistilBertModel, Tokenizer)> {
 
-struct Model {
-    first: Tensor,
-    second: Tensor,
+    const MODEL : &str = "";
+
+    let device = Device::Cpu;
+
+    let model_id = MODEL.to_string();
+
+    let revision = "main".to_string();
+
+    let repo = Repo::with_revision(model_id, RepoType::Model, revision);
+
+    let api = Api::new()?;
+
+    let api = api.repo(repo);
+
+    let config_file = api.get("config.json")?;
+
+    let tokenizer_file = api.get("tokenizer.json")?;
+
+    let config = std::fs::read_to_string(config_file)?;
+
+    let mut config: Config = serde_json::from_str(&config)?;
+
+    let tokenizer = Tokenizer::from_file(tokenizer_file).map_err(Error::msg)?;
+
+    let vb = match api.get("model.safetensors") {
+        Ok(path) => {
+            unsafe { VarBuilder::from_mmaped_safetensors(&[path], DType::F32, &device) }
+        },
+        Err(_) => {
+
+        }
+    }?;
+
+    let model = DistilBertModel::load(vb, &config)?;
+
+    Ok((model,tokenizer)) 
 }
 
-impl Model {
-    fn forward(&self, image: &Tensor) -> Result<Tensor> {
-        let x = image.matmul(&self.first)?;
-        let x = x.relu()?;
-        x.matmul(&self.second)
-    }
+fn embed() -> Result<()> {
+    Ok(())
 }
 
 fn main() -> Result<()> {
@@ -25,7 +59,7 @@ fn main() -> Result<()> {
 
     info!("Starting {pkg} v{ver}");
 
-    let start = std::time::Instant::now();
+     
 
     Ok(())
 }
